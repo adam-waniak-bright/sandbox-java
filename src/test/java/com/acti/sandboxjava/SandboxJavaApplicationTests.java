849009f47609fc25f60d7547a857036e9af5acb6
Adam Waniak <adamwaniak@bightinventions.pl>
2025-05-28 11:09:11 +0200

    Init commit

---
+++ src/test/java/com/acti/sandboxjava/SandboxJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acti.sandboxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
