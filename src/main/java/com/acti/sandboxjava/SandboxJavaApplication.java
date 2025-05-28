4967aba7ee3fe5e92d0dc2e7a0a83f7226cddf19
Adam Waniak <adamwaniak@bightinventions.pl>
2025-05-28 11:09:11 +0200

    Init commit

---
+++ src/main/java/com/acti/sandboxjava/SandboxJavaApplication.java
@@ -0,0 +1,13 @@
+package com.acti.sandboxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SandboxJavaApplication.class, args);
+  }
+
+}
