201d5ab6986cdda148df4d063c7934712532fc72
Sachin23102 <sachin.sanjay@anywhere.co>
2025-06-29 21:28:05 +0530

    feat: changes

---
+++ ordermanagement/src/test/java/com/quest/ordermanagement/OrdermanagementApplicationTests.java
@@ -0,0 +1,11 @@
+package com.quest.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagementApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
