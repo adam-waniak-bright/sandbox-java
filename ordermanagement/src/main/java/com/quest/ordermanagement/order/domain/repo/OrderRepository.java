6d4f6c16e9916eabc583fcd877502f6e7247227d
Sachin23102 <sachin.sanjay@anywhere.co>
2025-07-03 21:54:37 +0530

    feat: implement list orders API with pagination

--- ordermanagement/src/main/java/com/quest/ordermanagement/order/domain/repo/OrderRepository.java
+++ ordermanagement/src/main/java/com/quest/ordermanagement/order/domain/repo/OrderRepository.java
@@ -3,7 +3,8 @@ package com.quest.ordermanagement.order.domain.repo;
 import jakarta.validation.constraints.NotNull;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface OrderRepository extends JpaRepository<OrderEntity, String> {
+public interface OrderRepository extends JpaRepository<OrderEntity, String>, JpaSpecificationExecutor<OrderEntity> {
     Optional<OrderEntity> findById(@NotNull String id);
 }
